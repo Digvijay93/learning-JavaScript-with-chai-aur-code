8b86c65bcabe23fa691cf263ff9201639c4d227c
Digvijay Dhakate <154001294+Digvijay93@users.noreply.github.com>
2023-12-16 20:19:52 +0000

    updated variable file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,6 +5,11 @@ const accountId = 144553366; //"const" variable once declared cant be changed.
 let accountEmail = "digvijay@gmail.com";
 var accountPassword = "123456";
 
+let accountState; // it will show "Undefined" in output, because you only declared it and not initialized value for ex: let accountState = "Nagpur".
+
+let accountBalance;// variable Declaration
+accountBalance = 165456435464354; // variable initialization
+
 accountCity = "Jaipur";// dont use this way to declare variable.
 
 // accountId = 2; this will throw error, because it is "const" variable.
@@ -17,12 +22,12 @@ accountCity = "Bhandara";// this is also variable declaration way in JS,but dont
 // console.log(accountId);
 
 /*
-Prefer Not To Use "var" variables.Because of Block scope and Functional Scope.
+Prefer Not To Use "var" variables.Because of Block scope and Functional Scope problems.
 So, only use "let" & "const" variables.
 */
 
 // "console.table" ==> to display all variables in table form.
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState, accountBalance]);
 
 // to show output simply type "log" and click enter 
 console.log(accountEmail);
